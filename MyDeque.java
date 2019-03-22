1599c11c74221abbf1dd1ee22637479f50d79046
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-03-22 10:17:31 -0400

    end of period

--- MyDeque.java
+++ MyDeque.java
@@ -1,7 +1,14 @@
+import java.util.*;
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
-
+  public static void main(String[] args) {
+      MyDeque<Integer> test = new MyDeque<Integer> ();
+      test.addFirst(2);
+      test.addLast(3);
+      System.out.println(test);
+      System.out.println(test.getLast());
+  }
   @SuppressWarnings("unchecked")
   public MyDeque(){
       data = (E[])new Object[10];
@@ -24,9 +31,12 @@ public class MyDeque<E>{
       for(int i = start; i < start + end; i++){
           output += data[i%data.length].toString();
       }
-      return output;
+      return "{" + output + "}";
   }
   public void addFirst(E element){
+      if(element == null){
+          throw new NullPointerException();
+      }
       if(size == data.length){
       resize();
       }
@@ -34,11 +44,14 @@ public class MyDeque<E>{
           start = size;
           data[size] = element;
       } else{
-          start--;
-          data[start] = element;
+          data[start-1] = element;
+          start++;
       }
   }
   public void addLast(E element){
+      if(element == null){
+          throw new NullPointerException();
+      }
       if(size == data.length){
       resize();
       }
@@ -46,21 +59,33 @@ public class MyDeque<E>{
       size++;
   }
   public E removeFirst(){
+      if(size == 0){
+          throw new NoSuchElementException();
+      }
       E hold = data[start];
       start = (start+1)%data.length;
       size--;
       return hold;
   }
   public E removeLast(){
+      if(size == 0){
+          throw new NoSuchElementException();
+      }
       E hold = data[end];
       end = (end-1)%data.length;
       size--;
       return hold;
   }
   public E getFirst(){
+      if(size == 0){
+          throw new NoSuchElementException();
+      }
       return data[start];
   }
   public E getLast(){
+      if(size == 0){
+          throw new NoSuchElementException();
+      }
       return data[end];
   }
   @SuppressWarnings("unchecked")
