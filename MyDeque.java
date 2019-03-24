4eff76940e755c4aa2224dedeb0f1e8f31d3fd8c
Joseph Lee <jlee01@stuy.edu>
2019-03-23 22:28:13 -0400

    hm tested everything seems working, but it doesnt work on ethans driver for some reason

--- MyDeque.java
+++ MyDeque.java
@@ -10,12 +10,24 @@ public class MyDeque<E>{
   }
   public static void main(String[] args) {
       MyDeque<Integer> test = new MyDeque<Integer> ();
+      test.addLast(3);
       test.addFirst(2);
       test.addLast(3);
+      test.addFirst(4);
       test.toDebug();
       System.out.println(test);
       System.out.println(test.size());
       System.out.println(test.getLast());
+      test = new MyDeque<Integer> ();
+      for (int i = 9; i > -1 ; i--){
+          test.addLast(i);
+      }
+      test.addFirst(10);
+      System.out.println(test);
+      test.removeFirst();
+      System.out.println(test);
+      test.removeLast();
+      System.out.println(test);
   }
   @SuppressWarnings("unchecked")
   public MyDeque(){
@@ -37,7 +49,10 @@ public class MyDeque<E>{
   public String toString(){
       String output = "";
       for(int i = start; i < start + size; i++){
-          output += data[i%data.length].toString();
+          output += data[i%data.length] + ",";
+      }
+      if (output.length() > 0){
+          output = output.substring(0,output.length()-1);
       }
       return "{" + output + "}";
   }
@@ -55,8 +70,12 @@ public class MyDeque<E>{
       if(size == data.length){
       resize();
       }
-      data[modulus((start-1*(size)),data.length)] = element;
-      start = modulus((start-1),data.length);
+      if(size == 0){
+          data[0] = element;
+      } else {
+          data[modulus((start-1),data.length)] = element;
+          start = modulus((start-1),data.length);
+      }
       size++;
   }
 
@@ -67,8 +86,12 @@ public class MyDeque<E>{
       if(size == data.length){
       resize();
       }
-      data[(end+1)%data.length] = element;
-      end = (end+1)%data.length;
+      if(size == 0){
+          data[0] = element;
+      } else {
+          data[(end+1)%data.length] = element;
+          end = (end+1)%data.length;
+      }
       size++;
   }
   public E removeFirst(){
